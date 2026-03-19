985d95a383fcc1a0a00192570c466ee6a23d9fb2
ARTEM-SERDIUK <serdiuk.artem@lll.kpi.ua>
2026-03-19 18:12:00 +0200

    Add files via upload

---
+++ 2lab/sql/modules_data.sql
@@ -0,0 +1,190 @@
+INSERT INTO Module_Types (type_name) VALUES 
+('Гармата'), ('Башта'), ('Двигун'), ('Шасі'), ('Радіостанція');
+
+INSERT INTO Modules (name, type_id, price) VALUES
+-- Renault FT
+('37 mm APX SA18 (FT)', 1, 0),
+('13.2 mm Hotchkiss mle. 1930 (FT)', 1, 500),
+('Renault FT Berliet', 2, 0),
+('ER 52', 5, 450),
+('Renault (FT)', 3, 0),
+('M26/27', 4, 0),
+
+-- Panzerjäger I
+('3,7 cm Pak (t) L/47', 1, 0),
+('4,7 cm Pak (t) L/43', 1, 3800),
+('FuG 2', 5, 0),
+('Krupp M311', 3, 0),
+('Maybach HL 38 TR', 3, 1450),
+('Panzerjäger I', 4, 0),
+('Panzerjäger I verstärkteketten', 4, 1100),
+
+-- Pz.Kpfw. I Ausf. C
+('2 cm Kw.K. 38', 1, 0),
+('2 cm Flak 38', 1, 4200),
+('7,92 mm Mauser E.W. 141', 1, 5600),
+('Pz.Kpfw. I Breda', 2, 0),
+('Pz.Kpfw. I Ausf. C', 2, 2100),
+('FuG 5', 5, 0),
+('FuG 7', 5, 3200),
+('Fu.Spr.Ger. "a"', 5, 4500),
+('Maybach HL 45 P', 3, 0),
+('Maybach HL 66 P', 3, 4800),
+('VK 6.01', 4, 0),
+('VK 6.02', 4, 3400),
+
+-- Matilda
+('QF 2-pdr Mk. X (A12)', 1, 0),
+('QF 2-pdr Mk. X-B', 1, 12300),
+('Matilda Mk. IIA', 2, 0),
+('Matilda Mk. IIA*', 2, 5800),
+('WS No. 19 Mk. II', 5, 0),
+('2x AEC', 3, 0),
+('2x Leyland E164', 3, 7200),
+('Matilda Mk. II', 4, 0),
+('Matilda Mk. IIA', 4, 6100),
+
+-- AMX ELC bis
+('75 mm SA44L', 1, 0),
+('90 mm D. 915', 1, 28000),
+('AMX ELC bis', 2, 0),
+('SCR 508', 5, 0),
+('SOFAM de 150 cv', 3, 0),
+('SOFAM de 180 cv', 3, 15400),
+('AMX ELC', 4, 0),
+('AMX ELC bis', 4, 11200),
+
+-- O-I
+('10 cm Cannon Type 92 (O-I)', 1, 0),
+('15 cm Howitzer Type 96 (O-I)', 1, 54000),
+('O-I', 2, 0),
+('Type 96 Mk. 4 Bo', 5, 0),
+('2x Kawasaki Type 98 Kou (O-I)', 3, 0),
+('2x Kawasaki Type 98 Otsu (O-I)', 3, 34000),
+('O-I', 4, 0),
+('O-I Kai', 4, 21200),
+
+-- Bishop
+('Q.F. 25-Pr. Gun/How. (Bishop)', 1, 0),
+('Q.F. 4.5-in. Howitzer (Bishop)', 1, 32000),
+('WS No. 19 Mk. III', 5, 0),
+('AEC A190 (Bishop)', 3, 0),
+('GMC 6004 6-71A (Bishop)', 3, 16500),
+('Valentine Mk. II', 4, 0),
+('Bishop', 4, 12600),
+
+-- Hotchkiss EBR
+('75 mm SA49 (EBR)', 1, 0),
+('Canon de 75 mm Vo 1000 m/s (EBR)', 1, 59500),
+('Hotchkiss EBR', 2, 0),
+('SCR-528 F', 5, 0),
+('Hotchkiss V6 mle. 48', 3, 0),
+('Hotchkiss V6 mle. 50', 3, 42000),
+('Hotchkiss EBR', 4, 0),
+('Hotchkiss EBR bis', 4, 33100),
+
+-- Panther
+('7,5 cm Kw.K. 42 L/70 Ausf. A', 1, 0),
+('7,5 cm Kw.K. 42 L/70 Ausf. G', 1, 62000),
+('Pz.Kpfw. Panther Ausf. G', 2, 0),
+('Pz.Kpfw. Panther Schmalturm', 2, 34000),
+('FuG 12', 5, 0),
+('Maybach HL 210 TRM P30 (G)', 3, 0),
+('Maybach HL 230 TRM P30 (G)', 3, 45000),
+('Pz.Kpfw. Panther Ausf. A', 4, 0),
+('Pz.Kpfw. Panther Ausf. G', 4, 25400),
+
+-- UDES 03
+('9 cm kan m/F', 1, 0),
+('10,5 cm kan UDES 03', 1, 115000),
+('Ra 421', 5, 0),
+('Saab-Scania DS14', 3, 0),
+('Leyland L60', 3, 72000),
+('Projekt 2013A', 4, 0),
+('UDES 03', 4, 51000),
+
+-- P.44 Pantera
+('Cannone da 90/53', 1, 0),
+('Cannone da 90/74', 1, 121000),
+('P.44 Pantera prima variante', 2, 0),
+('P.44 Pantera seconda variante', 2, 48000),
+('R.F. 4 M.', 5, 0),
+('Alfa RA1000I C.A.', 3, 0),
+('FIAT RA1050I C.A.', 3, 75000),
+('P.44 Pantera', 4, 0),
+('P.44 Pantera v.s.', 4, 46000),
+
+-- TNH 105/1000
+('105 mm vz. 38N', 1, 0),
+('105 mm vz. 38N (a)', 1, 142000),
+('105 mm vz. 40N (1000)', 1, 185000),
+('TNH 105/1000 první model', 2, 0),
+('TNH 105/1000 druhý model', 2, 62000),
+('Radiostanice RM-31T', 5, 0),
+('ČKD AXK proto', 3, 0),
+('Škoda V16 AHK-2', 3, 105000),
+('TNH 105/1000 (1947)', 4, 0),
+('TNH 105/1000 (1948)', 4, 61000),
+
+-- M53/M55
+('155 mm Gun M46', 1, 0),
+('8-inch Howitzer M47', 1, 210000),
+('AN/PRC-25', 5, 0),
+('Ford GAC', 3, 0),
+('Continental AV-1790-7B', 3, 115000),
+('T97/T108', 4, 0),
+('M53/M55', 4, 65000),
+
+-- Tortoise
+('OQF 32-pdr AT Gun (A39)', 1, 0),
+('120 mm AT Gun L1A1', 1, 245000),
+('SR C42', 5, 0),
+('Rolls-Royce Meteor Mk. V', 3, 0),
+('Rolls-Royce Meteor M120 (A39)', 3, 98000),
+('Tortoise', 4, 0),
+('Tortoise Mk. 2', 4, 63000),
+
+-- Standard B
+('Cannone da 90 Rh', 1, 0),
+('Cannone da 105 Rh V1', 1, 195000),
+('Prototipo Standard A (W 2)', 2, 0),
+('Prototipo Standard B (R 1)', 2, 61000),
+('RV 4', 5, 0),
+('MB 837 Aa', 3, 0),
+('MB 837 Ea', 3, 102000),
+('Prototipo Standard B I', 4, 0),
+('Prototipo Standard B II', 4, 62000),
+
+-- CS-63
+('105 mm armata wz. 64', 1, 0),
+('CS-63', 2, 0),
+('R-123', 5, 0),
+('SGT-3CzM', 3, 0),
+('CS-63', 4, 0),
+
+-- Manticore
+('QF 105 mm Gun', 1, 0),
+('Manticore 1955', 2, 0),
+('C42 VHF', 5, 0),
+('Rolls-Royce B81 (1955)', 3, 0),
+('Manticore 1955', 4, 0),
+
+-- Rinoceronte
+('127 mm OTO Melara', 1, 0),
+('Rinoceronte', 2, 0),
+('SEM-25', 5, 0),
+('Daimler–Benz Typ MB 838', 3, 0),
+('Rinoceronte', 4, 0),
+
+-- NC 70 Błyskawica
+('GG-85/175 mm', 1, 0),
+('R-123', 5, 0),
+('2x GTD-2T6', 3, 0),
+('NC 70 Błyskawica', 4, 0),
+
+-- leKpz Borkenkäfer
+('105 mm Rh 105-20', 1, 0),
+('leKpz Borkenkäfer', 2, 0),
+('SEM 25BK', 5, 0),
+('MAN D2840 LXE', 3, 0),
+('leKpz Borkenkäfer', 4, 0);
